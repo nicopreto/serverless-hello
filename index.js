e1ae79b879ef8579cc40ef74d570bc7b13857e2d
Nico Pretorius <npretorius@scottlogic.com>
2024-01-09 10:02:27 +0000

    change return message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `hello ${name}`
+        message: `hello ${name}!`
       },
       null,
       2
